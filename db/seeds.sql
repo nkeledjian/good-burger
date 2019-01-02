9047500b75492488a5f3634c4dacd5049dd908f9
nVar <nkeledjian@gmail.com>
2019-01-02 15:19:03 -0800

    more initial files - testing sql server files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (id, burger_name, devoured)
+VALUES ("1", "Veggie Supreme Burger", NULL),
+("2", "Bacon Cheese Burger", NULL),
+("3", "Grilled Chicken Burger", NULL),
+("4", "Impossible Burger", NULL),
+("5", "Bison Burger", NULL),
