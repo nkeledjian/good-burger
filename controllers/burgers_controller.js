d5387ac2d8459fad1644396aa9f3709e00389b73
nVar <nkeledjian@gmail.com>
2019-01-12 20:30:11 -0800

    attempting 503 error and SQL syntax error

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -48,19 +48,19 @@ var burger = require("../models/burger.js");
   });
   // May implement future burger deleting functionality
   
-  // router.delete("/api/burgers/:id", function(req, res) {
-  //   var condition = "id = " + req.params.id;
+  router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
   
-  //   burger.delete(condition, function(result) {
-  //     if (result.affectedRows == 0) {
-  //       // If no rows were changed, then the ID must not exist, so 404 error and end query connection
-  //       return res.status(404).end();
-  //     } else {
-  //       // 200 code = successful connection, then end query connection
-  //       res.status(200).end();
-  //     }
-  //   });
-  // });
+    burger.delete(condition, function(result) {
+      if (result.affectedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404 error and end query connection
+        return res.status(404).end();
+      } else {
+        // 200 code = successful connection, then end query connection
+        res.status(200).end();
+      }
+    });
+  });
   
   // Export routes for server.js to use.
   module.exports = router;
